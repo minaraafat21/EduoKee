f7ec433cc2b7b0ac588911f9ffde028aa09a097f
mina <minaraafatemil14@gmail.com>
2024-06-07 00:29:26 +0300

    Updates

---
+++ precache-manifest.b4d58c0dd148e33cc60bbeecb9d70f4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461ed4ce3618964baddd4f005ca8859e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d46aad530d98028fa24b",
+    "url": "/static/css/2.48d95ff0.chunk.css"
+  },
+  {
+    "revision": "dfa295c0fcb92b4c7c2e",
+    "url": "/static/css/main.bd2f1f27.chunk.css"
+  },
+  {
+    "revision": "d46aad530d98028fa24b",
+    "url": "/static/js/2.14e3f279.chunk.js"
+  },
+  {
+    "revision": "6aea8f39c38554b25bc6",
+    "url": "/static/js/3.78ea13c0.chunk.js"
+  },
+  {
+    "revision": "dfa295c0fcb92b4c7c2e",
+    "url": "/static/js/main.5d4fba08.chunk.js"
+  },
+  {
+    "revision": "70460fe9099122a5fcf0",
+    "url": "/static/js/runtime~main.acd8d5bd.js"
+  },
+  {
+    "revision": "fa63b6aeb8eadfec5055b590ca4f32bb",
+    "url": "/static/media/Eduokee.fa63b6ae.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
